904f9e3e55385c8cb234b5f6b72842d4699e157d
nil kaneriya <kaneriyaneel2020@gmail.com>
2024-08-23 20:09:49 +0530

    addd

--- src/landing_page/about/Team.js
+++ src/landing_page/about/Team.js
@@ -8,7 +8,7 @@ function Team() {
 
             <div className='row p-3 text-muted fs-60' style={{ lineHeight: "1.8", fontSize: "1.2rem" }}>
                 <div className='col-6 p-5 text-center'>
-                    <img src='media/images/nithinKamath.jpg' style={{ borderRadius: "100%", width: "50%" }} />
+                    <img src='media/images/nithinKamath.jpg' style={{ borderRadius: "100%", width: "50%" }} alt='ima'/>
                     <h4 className='mt-5'>Nithin Kamath</h4>
                     <h6>Founder, CEO</h6>
                 </div>
@@ -23,7 +23,7 @@ function Team() {
                     Playing basketball is his zen.
                     </p>
                     <p>
-                    Connect on<a href='' style={{ textDecoration: "none" }}>Homepage</a> / <a href='' style={{ textDecoration: "none" }}>TradingQnA </a>/ <a href='' style={{ textDecoration: "none" }}>Twitter </a>
+                    Connect on<a href='https://www.apnacollege.in/start' style={{ textDecoration: "none" }}>Homepage</a> / <a href='https://www.apnacollege.in/start' style={{ textDecoration: "none" }}>TradingQnA </a>/ <a href='https://www.apnacollege.in/start' style={{ textDecoration: "none" }}>Twitter </a>
                     </p>
                     
                 </div>
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -5,7 +5,7 @@ function Hero() {
     <section className="container-fluid" id="supportHero">
       <div className="p-5 " id="supportWrapper">
         <h4>Support Portal</h4>
-        <a href="">Track Tickets</a>
+        <a href="https://www.apnacollege.in/start">Track Tickets</a>
       </div>
       <div className="row p-5 m-3">
         <div className="col-6 p-3">
@@ -14,10 +14,10 @@ function Hero() {
           </h1>
           <input placeholder="Eg. how do I activate F&O" />
           <br />
-          <a href="">Track account opening</a>
-          <a href="">Track segment activation</a>
-          <a href="">Intraday margins</a>
-          <a href="">Kite user manual</a>
+          <a href="https://www.apnacollege.in/start">Track account opening</a>
+          <a href="https://www.apnacollege.in/start">Track segment activation</a>
+          <a href="https://www.apnacollege.in/start">Intraday margins</a>
+          <a href="https://www.apnacollege.in/start">Kite user manual</a>
         </div>
         <div className="col-6 p-3">
           <h1 className="fs-3">Featured</h1>
